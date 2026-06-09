4545a2a87407974f5d33463e6375173973b64efe
khush-aziro <kagarwal@aziro.com>
2026-06-09 11:48:54 +0530

    refactor(EditResume): migrate Context.Provider to Context-as-component for React 19

--- src/dashboard/resume/[resumeId]/edit/index.jsx
+++ src/dashboard/resume/[resumeId]/edit/index.jsx
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import FormSection from '../../components/FormSection';
 import ResumePreview from '../../components/ResumePreview';
 import { ResumeInfoContext } from '@/context/ResumeInfoContext';
-import dummy from '@/data/dummy';
 import GlobalApi from './../../../../../service/GlobalApi';
 
 function EditResume() {
@@ -23,14 +22,14 @@ function EditResume() {
     }
 
   return (
-    <ResumeInfoContext.Provider value={{resumeInfo,setResumeInfo}}>
+    <ResumeInfoContext value={{resumeInfo,setResumeInfo}}>
     <div className='grid grid-cols-1 md:grid-cols-2 p-10 gap-10'>
         {/* Form Section  */}
           <FormSection/>
         {/* Preview Section  */}
          <ResumePreview/>
     </div>
-    </ResumeInfoContext.Provider>
+    </ResumeInfoContext>
   )
 }
 
